23094457c8ec51531224543a58ca0145dd8d46be
phammerr <ethananpham@gmail.com>
2023-04-23 23:10:52 -0400

    changed to libraries

--- script.js
+++ script.js
@@ -1,43 +1,139 @@
 /*
-API
-https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson
-https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
 */
-
-/*
-function initMap() {
-  const carto = L.map("map").setView([37.8, -96], 4);
-  L.titleLayer("https://.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  }).addTo(carto);
-  return carto;
-}
-
-
-
-async function mainEvent() {
-  const carto = initMap();
-
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
   
-  fetch(
-    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const caliEarthquakes = data.features.filter((feature) => {
-        const place = feature.properties.place;
-        return /california/i.test(place);
-      });
+  function injectHTML(list) {
+    console.log('fired injectHTML');
+    const target = document.querySelector('#restaurant_list');
+    target.innerHTML = '';
+    list.forEach((item) => {
+      const str = `<li>${item.branch_name}</li>`;
+      target.innerHTML += str;
     });
-}
+  }
+  /* A quick filter that will return something based on a matching input */
+  function filterList(list, query) {
+    return list.filter((item) =>{
+      const lowerCaseName = item.branch_name.toLowerCase(); 
+      const lowerCaseQuery = query.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    });
+    /*
+      Using the .filter array method, 
+      return a list that is filtered by comparing the item name in lower case
+      to the query in lower case
+      Ask the TAs if you need help with this
+    */
+  }
+  
+  function cutRestaurantList(list){
+    console.log('fired cut list');
+    const range =[...Array(15).keys()];
+    return newArray = range.map((item)=> {
+      const index = getRandomIntInclusive(0, list.length - 1);
+      return list[index];
+    });
+  }
+  
+  function initMap(){
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto;
+  }
 
-document.addEventListener("DOMContentLoaded", async () => mainEvent());
-*/
+  function markerPlace(array, map){
+    console.log('array for markers', array);
 
-/*fetching API using d3.js*/
-d3.json("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02
-).then(function(data){
-  console.log(data)
-});
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+    
+    array.forEach((item) => {
+        console.log('marker place', item);
+        const{latitude} = item.location_1
+        const{longitude} = item.location_1
+        L.marker([latitude, longitude]).addTo(map);
+    })
+  }
 
+  async function mainEvent() { // the async keyword means we can make API requests
+    const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+    const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
+    const generateListButton = document.querySelector('#generate');
+    const textField = document.querySelector('#resto');
+
+    const loadAnimation = document.querySelector('#data_load_animation');
+    loadAnimation.style.display = 'none';
+    generateListButton.classList.add('hidden');
+
+    const carto = initMap();
+
+    const storedData = localStorage.getItem("storedData");
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0) {
+        generateListButton.classList.remove("hidden");
+    }
+
+    let currentList = []; // this is "scoped" to the main event function
+    
+    /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
+    loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+      
+      // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
+      // submitEvent.preventDefault(); 
+      
+      // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
+      console.log('Loading data'); 
+      loadAnimation.style.display = 'inline-block';
+      // Basic GET request - this replaces the form Action
+      const results = await fetch('https://data.princegeorgescountymd.gov/resource/7k64-tdwr.json');
+  
+      // This changes the response from the GET into data we can use - an "object"
+      const storedList = await results.json();
+      localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList;
+    
+      if (parsedData?.length > 0) {
+        generateListButton.classList.remove("hidden");
+    }
+
+      loadAnimation.style.display = 'none';
+    // console.table(storedList); 
+    });
+  
+    generateListButton.addEventListener('click', (event) => {
+      console.log('generate new list');
+      currentList = cutRestaurantList(parsedData);
+      console.log(currentList);
+      injectHTML(currentList);
+      markerPlace(currentList, carto);
+    });
+
+    textField.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      const newList = filterList(currentList, event.target.value);
+      injectHTML(newList);
+      console.log(newList);
+      markerPlace(newList, carto);
+    });
+  
+    clearDataButton.addEventListener("click", (event) => {
+        console.log('clear browser data');
+        localStorage.clear();
+        console.log('localStorage Check', localStorage.getItem("storedData"));
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
