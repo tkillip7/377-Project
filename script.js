a8aaac8d36b569823df6bac6af74c01035ae1c6e
tkillip7 <tkp3170@gmail.com>
2023-04-23 20:45:14 -0400

    first attempt at map

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 /*
-    Adding the earthquake database
+API
+https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson
+https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02
 */
 
-d3.json(
-  "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02"
-).then(function (data) {
-  console.log(data);
-});
+function initMap() {
+  const carto = L.map("map").setView([37.8, -96], 4);
+  L.titleLayer("https://.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  }).addTo(carto);
+  return carto;
+}
+
+/*function to filter database to states in Cali?*/
+
+async function mainEvent() {
+  const carto = initMap();
+
+  /*API request */
+  fetch(
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const caliEarthquakes = data.features.filter((feature) => {
+        const place = feature.properties.place;
+        return /california/i.test(place);
+      });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", async () => mainEvent());
